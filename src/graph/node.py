f0df444dbd96bccdee39ab86b04f0ab22019d2a7
Mariana <rc.mariana.01@gmail.com>
2024-12-29 16:06:26 +0000

    feat: priority zone added

--- src/graph/node.py
+++ src/graph/node.py
@@ -1,8 +1,9 @@
 class Node:
-    def __init__(self, position):
+    def __init__(self, position, priority):
         self.position = position
         self.neighbours = []  # List of tuples (Node, open: bool)
         self.accessible_terrains = [0,1,2] # List of Terrain
+        self.priorityZone = 0
 
     def can_access_terrain(self, terrain):
         return terrain in self.accessible_terrains
